4b78a235ada9f0567f5038cf9c20c7f463274104
Mo <moh19951@outlook.com>
2020-03-10 10:06:24 +0100

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -11,36 +11,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if(empty(trim($_POST["username"]))) {
         $username_err = "Vul een gebruikersnaam in.";
-    } else{
+    } else {
 
         $sql = "SELECT id FROM users WHERE username = ?";
 
-        if($stmt = mysqli_prepare($link, $sql)) {
-
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-
-            $param_username = trim($_POST["username"]);
-
-
-            if(mysqli_stmt_execute($stmt)) {
-
-                mysqli_stmt_store_result($stmt);
-
-                if(mysqli_stmt_num_rows($stmt) == 1) {
-                    $username_err = "Deze gebruikersnaam bestaat al.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Er is iets mis gegaan.";
-            }
-
-
-            mysqli_stmt_close($stmt);
-        }
     }
-
 
     if(empty(trim($_POST["password"]))) {
         $password_err = "Vul aub een wachtwoord in.";
--- login.php
+++ login.php
@@ -37,59 +37,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
 
-        if($stmt = mysqli_prepare($link, $sql)) {
-
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-
-            $param_username = $username;
-
-
-            if(mysqli_stmt_execute($stmt)) {
-
-                mysqli_stmt_store_result($stmt);
-
-
-                if(mysqli_stmt_num_rows($stmt) == 1) {
-
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)) {
-                        if(password_verify($password, $hashed_password)) {
-
-                            session_start();
-
-
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
-
-                            header("location: welcome.php");
-                        } else{
-
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-
-    mysqli_close($link);
-}
+    }}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
